e300355bf358818e922ea56ad4ec7a8698c556cd
Paul Serraino <pserraino99@gmail.com>
2015-09-14 11:17:29 -0500

    config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
 module.exports = {
   redis: require('./redis')
+, workers: 4        // # of cpus
+, limit: 1          // default concurrency for all jobs
+, timeout: 10*1000  // run jobs every 10 seconds
 };
