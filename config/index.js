f1a11934c375a0fc1ac96b7965f479105202920e
paulserraino <pserraino99@gmail.com>
2015-09-23 16:52:31 -0500

    scale down cpus

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   redis: require('./redis')
-, workers: 6        // # of cpus
+, workers: 2        // # of cpus
 , limit: 4          // default concurrency for all jobs
 , timeout: 10*1000  // run jobs every 10 seconds
 };
