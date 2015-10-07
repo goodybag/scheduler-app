e8685b42c070e92929851702adb6131ab6aaa202
paulserraino <pserraino99@gmail.com>
2015-10-07 13:14:59 -0500

    init scheduler ui

--- config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  redis: require('./redis')
-, workers: 2        // # of cpus
-, limit: 4          // default concurrency for all jobs
-, timeout: 10*1000  // run jobs every 10 seconds
-};
