9fdaf4a7b5eb6469ec3e48316cd4e2adf8d38204
paulserraino <pserraino99@gmail.com>
2015-10-08 15:54:34 -0500

    using port 8000

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var app = require('./app');
 var http = require('http');
 
 var server = http.createServer(app());
-var port = process.argv[2] || 3000;
+var port = process.argv[2] || 8000;
 server.listen(port);
 console.log('listening on port', port);
