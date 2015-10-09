979d4050210afc3711f25f8e6fe9a0ae84147fa7
Paul Serraino <pserraino99@gmail.com>
2015-10-09 11:23:27 -0500

    scheduler api endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var utils = require('../utils');
 var router = express.Router();
 
+/*
+* GET /api/scheduler
+*/
+
 router.get('/scheduler', function(req, res) {
-    var pgquery = req.app.get('query');
+  var pgquery = req.app.get('query');
 
   var query = {
     type: 'select'
@@ -11,6 +16,50 @@ router.get('/scheduler', function(req, res) {
       status: 'pending'
     }
   };
+
+  pgquery(query, function (error, results) {
+    if (error) return res.status(500).json(error);
+    res.json(results);
+  });
+});
+
+/*
+* GET /api/scheduler/:job_id
+*/
+
+router.get('/scheduler/:job_id', function(req, res) {
+  var pgquery = req.app.get('query');
+
+  var query = {
+    type: 'select'
+  , table: 'scheduled_jobs'
+  , where: {
+      id: req.params.job_id
+    }
+  };
+
+  pgquery(query, function (error, results) {
+    if (error) return res.status(500).json(error);
+    res.json(results);
+  });
+});
+
+/*
+* PUT /api/scheduler/:job_id
+*/
+
+router.put('/scheduler/:job_id', function(req, res) {
+  var pgquery = req.app.get('query');
+
+  var query = {
+    type: 'update'
+  , table: 'scheduled_jobs'
+  , where: {
+      id: req.params.job_id
+    }
+  };
+
+  query.values = utils.accept(utils.extend(req.query, req.body), ['status']);
 
   pgquery(query, function (error, results) {
     if (error) return res.status(500).json(error);
